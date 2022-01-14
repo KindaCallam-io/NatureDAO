4edb104cdf35fc16829bb1b4091f0c28002fc83f
Callam Ingram <callamingram@Callams-MBP.home>
2022-01-13 16:20:29 -0800

    Fixed Navbar to the top of the using <Navbar fixed='top'>

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import Head from 'next/head'
-// import Navbar from 'react-bootstrap/Navbar';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav'; 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
     <div className="container">
+
+      <Navbar fixed="top" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">NatureDAO</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+
+      
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -51,6 +64,8 @@ export default function Home() {
           </a>
         </div>
       </main>
+
+      
 
       <footer>
         <a
